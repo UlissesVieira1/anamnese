eb63e477f178da3e5327359cd78e7dea2e8ab9d3
udabliu <solgamsao@gmail.com>
2026-01-06 19:20:02 -0300

    subindo ajustes

--- anamnese-api/src/anamnese/anamnese.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AnamneseController } from './anamnese.controller';
-import { AnamneseService } from './anamnese.service';
-
-@Module({
-  controllers: [AnamneseController],
-  providers: [AnamneseService]
-})
-export class AnamneseModule {}
--- anamnese-api/src/anamnese/anamnese.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AnamneseService {
-  inserirDadosAnamnese(data: any) {
-    console.log('Dados recebidos:', data);
-    return { ok: true };
-  }
-}
